c933b1803426bcf05300c3495e1025680ac9e56d
Delta-Leiras <138731060+Israel-Henrique@users.noreply.github.com>
2024-01-13 22:41:31 -0300

    Desafio concluído
    Fiz as alterações, desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Smartphone celular = new Iphone("4002-8922", "iPhone SE", "o que é isso?", 99);
+        celular.Ligar();
+        celular.ReceberLigacao();
+        celular.InstalarAplicativo("Sonic Runners");
+        celular = new Nokia("0500 2012-007", "para doar 7 reais", "ainda não sei o que é isso", 8);
+        celular.InstalarAplicativo("Skyline Skaters");
+    }
+}
